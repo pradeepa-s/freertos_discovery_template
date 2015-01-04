bbfe1f97c9de1575b16196493479d1423714cb01
Pradeepa Senanayake <pradeepa.kck@gmail.com>
2015-01-04 17:19:00 +0530

    Project commit.

---
+++ Keil/Source/main.c
@@ -0,0 +1,107 @@
+//******************************************************************************
+#include "stm32f4xx.h"
+#include "stm32f4xx_conf.h"
+#include "discoveryf4utils.h"
+//******************************************************************************
+
+//******************************************************************************
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "task.h"
+#include "croutine.h"
+//******************************************************************************
+
+void vLedBlinkBlue(void *pvParameters);
+void vLedBlinkRed(void *pvParameters);
+void vLedBlinkGreen(void *pvParameters);
+void vLedBlinkOrange(void *pvParameters);
+//void Delay(uint32_t val);
+
+#define STACK_SIZE_MIN	128	/* usStackDepth	- the stack size DEFINED IN WORDS.*/
+
+//******************************************************************************
+int main(void)
+{
+	/*!< At this stage the microcontroller clock setting is already configured,
+	   this is done through SystemInit() function which is called from startup
+	   file (startup_stm32f4xx.s) before to branch to application main.
+	   To reconfigure the default setting of SystemInit() function, refer to
+	   system_stm32f4xx.c file
+	 */
+	
+	/*!< Most systems default to the wanted configuration, with the noticeable 
+		exception of the STM32 driver library. If you are using an STM32 with 
+		the STM32 driver library then ensure all the priority bits are assigned 
+		to be preempt priority bits by calling 
+		NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 ); before the RTOS is started.
+	*/
+	NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 );
+	
+	STM_EVAL_LEDInit(LED_BLUE);
+	STM_EVAL_LEDInit(LED_GREEN);
+	STM_EVAL_LEDInit(LED_ORANGE);
+	STM_EVAL_LEDInit(LED_RED);
+	
+	xTaskCreate( vLedBlinkBlue, (const signed char*)"Led Blink Task Blue", 
+		STACK_SIZE_MIN, NULL, tskIDLE_PRIORITY, NULL );
+	xTaskCreate( vLedBlinkRed, (const signed char*)"Led Blink Task Red", 
+		STACK_SIZE_MIN, NULL, tskIDLE_PRIORITY, NULL );
+	xTaskCreate( vLedBlinkGreen, (const signed char*)"Led Blink Task Green", 
+		STACK_SIZE_MIN, NULL, tskIDLE_PRIORITY, NULL );
+	xTaskCreate( vLedBlinkOrange, (const signed char*)"Led Blink Task Orange", 
+		STACK_SIZE_MIN, NULL, tskIDLE_PRIORITY, NULL );
+	
+	vTaskStartScheduler();
+   
+//    while(1)
+//    {
+//       STM_EVAL_LEDToggle(LED_BLUE);
+//       Delay(0xFFFFFF);
+//       STM_EVAL_LEDToggle(LED_BLUE);
+//       Delay(0xFFFFFF);
+//    }
+}
+//******************************************************************************
+
+// void Delay(uint32_t val)
+// {
+//    while(val--); 
+// }
+
+//******************************************************************************
+void vLedBlinkBlue(void *pvParameters)
+{
+	for(;;)
+	{
+		STM_EVAL_LEDToggle(LED_BLUE);
+		vTaskDelay( 1000 / portTICK_RATE_MS );
+	}
+}
+
+void vLedBlinkRed(void *pvParameters)
+{
+	for(;;)
+	{
+		STM_EVAL_LEDToggle(LED_RED);
+		vTaskDelay( 500 / portTICK_RATE_MS );
+	}
+}
+
+void vLedBlinkGreen(void *pvParameters)
+{
+	for(;;)
+	{
+		STM_EVAL_LEDToggle(LED_GREEN);
+		vTaskDelay( 200 / portTICK_RATE_MS );
+	}
+}
+
+void vLedBlinkOrange(void *pvParameters)
+{
+	for(;;)
+	{
+		STM_EVAL_LEDToggle(LED_ORANGE);
+		vTaskDelay( 300 / portTICK_RATE_MS );
+	}
+}
+//******************************************************************************
